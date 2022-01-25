6325d875697806379bf3b09818046c0e204033c2
Nithi1701 <ballnithilampay1@gmail.com>
2022-01-25 10:44:31 +0700

    Lac

---
+++ bassic-template/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  const headingStyle = 'color:Green'
+  const paraStyle = 'text-decoration:underline'
+  const imgPath = '../public/favicon.ico'
+</script>
+ 
+<template>
+  <h1 :style="headingStyle">Client 2</h1>
+  <h2 :style="paraStyle">Lec by Nithi</h2>
+  <!-- รูปตกแต่งไว้ใน assets -->
+  <img src="./assets/logo.png">
+  <img :src="imgPath">
+</template>
+ 
+<style>
+
+</style>
