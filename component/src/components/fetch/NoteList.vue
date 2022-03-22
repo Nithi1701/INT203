c6349f8fdd7610ae11fd999a11b6712eb66f9123
nithi63130500072 <nithi.1701@mail.kmutt.ac.th>
2022-03-22 09:10:49 +0700

    fetch-vue

---
+++ component/src/components/fetch/NoteList.vue
@@ -0,0 +1,30 @@
+<script setup>
+// delete
+    defineEmits(['deleteNote', 'editNote'])
+// get
+    defineProps({
+        noteList: {
+            type: Array,
+            require: true
+        }
+    })
+</script>
+
+<template>
+  <div>
+    <h2>Note List</h2>
+    <ul>
+      <li v-for="(note, index) in noteList" :key="index">
+        Note Id: {{ note.id }}, Note Detail: {{ note.noteDetail }}
+        <!-- edit -->
+        <button @click="$emit('editNote', note)">Edit</button>
+        <!-- delete -->
+        <button @click="$emit('deleteNote', note.id)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+
+</style>
